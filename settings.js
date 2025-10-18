766d096865c01061246dce5737f092a34cdc9ee9
terrizevdev <terrizevdev@gmail.com>
2025-10-18 13:28:54 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: {
+        main: 'terri API !!',
+        copyright: 'Terri'
+    },
+    description: 'Simple API with easy and minimalistic integration for WhatsApp Bot Developers.',
+    icon: '/image/icon.png',
+    author: 'Terri',
+    info_url: 'https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y',
+    links: [
+        {
+            name: 'WhatsApp Information Ch.',
+            url: 'https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y'
+        }
+    ],
+    // New: Global maintenance mode
+    maintenance: false,
+    
+    // New: Individual API status
+    apiStatus: {
+        'youtube': 'online',
+        'ytmp3': 'online', 
+        'ytmp4': 'online',
+        'snackvideo': 'online',
+        'android1': 'online',
+        'apkcombo': 'online'
+        // Add more APIs as needed
+    }
+};
